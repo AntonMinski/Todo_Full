b794232e77f8a6df207847b1d1816c1deffa77d0
AntonMinski <Anton_minski5@ukr.net>
2021-10-12 14:22:07 +0300

    014_2 pagination in separate component

--- task-frontend/src/components/Task/FilterTask.jsx
+++ task-frontend/src/components/Task/FilterTask.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useCallback, useState } from 'react'
 import classes from './FilterTask.module.css'
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch} from 'react-redux';
 
 import { getTasksAction } from '../../store/tasks-slice';
+
+import PaginationTask from './Pagination';
 
 
 
@@ -23,7 +25,7 @@ const FilterTask = () => {
 
   useEffect(() => {
     getTasks()
-  }, [title, getTasks]);
+  }, [getTasks]);
 
   
   const handleChangeTitle = e => {
@@ -34,9 +36,7 @@ const FilterTask = () => {
     setStatus(e.target.value);
   };
 
-
   // Pagination :
-
 
   const handleChangeLimit = e => {
     setLimit(e.target.value);
@@ -44,46 +44,7 @@ const FilterTask = () => {
     setPage(1)
   };
 
-  const total = useSelector(state => state.tasks.total);
-  const totalPages = Math.ceil(total / limit)
-
-  const maxMiddle = (Math.ceil(totalPages / 5 - 1) * 5) + 3
-
-  function goToFirstPage() {
-    setMiddle(3);
-    setPage(1);
-  }
-
-  function goToPreviousPage() {
-    setMiddle((middle) => Math.max(3, middle - 5));
-  }
-
-  function goToNextPage() {
-    setMiddle((middle) => Math.min(maxMiddle, middle + 5))
- 
-  }
-
-  function goToLastPage() {
-    setMiddle(maxMiddle);
-    setPage(totalPages);
-  }
-
-  function changePage(event) {
-    const pageNumber = Number(event.target.textContent);
-    setPage(pageNumber);
-  }
-
-  const getPaginationGroup = useCallback(() => {
-    const start = Math.max(1, middle - 2)
-    const end = Math.min(middle + 2, totalPages)
-    let list = [];
-    for (let i = start; i <= end; i++) {
-      list.push(i);
-    }
-    return list
-  }, [middle, totalPages]);
-
-
+  
   return (
     <div>
     <form className={classes.Task_search}>
@@ -107,62 +68,7 @@ const FilterTask = () => {
     
      {/* pagination */}
 
-     {limit && (
-       <div className={classes.pagination}>
-
-       {/* first page button */}
-       { middle > 3 && (
-         <button
-         onClick={goToFirstPage}
-         className={`${classes.paginationItem} 'active' `}        >
-         <span>1</span>
-         </button>
-       )}
- 
- 
-       {/* previous button */}
-       {middle > 3 &&(
-         <button
-         onClick={goToPreviousPage}
-         className={`${classes.prev} ${page === 1 ? 'disabled' : ''}`}
-       >
-         &lt;&lt;
-       </button>
-       )}       
- 
-       {/* show page numbers */}
-       {getPaginationGroup().map((item, index) => (
-         <button
-           key={index}
-           onClick={changePage}
-           className={`${classes.paginationItem} ${page === item ? 'active' : null}`}
-         >
-           <span>{item}</span>
-         </button>
-       ))}
- 
-       {/* next button */}
-
-       {middle < maxMiddle && (
-         <button
-         onClick={goToNextPage}
-         className={`${classes.next} ${page === totalPages ? 'disabled' : ''}`} >
-          &gt;&gt;
-       </button>
-      )}
-       
- 
-       {/* last page button */}
-       { middle < totalPages && (
-         <button
-         onClick={goToLastPage}
-         className={`${classes.paginationItem} 'active'`}        >
-         <span>{totalPages}</span>
-         </button>
-       )}
- 
-     </div>
-     )}
+      <PaginationTask page={page} setPage={setPage} limit={limit} middle={middle} setMiddle={setMiddle} />
 
     </div>
   )
---
+++ task-frontend/src/components/Task/Pagination.jsx
@@ -0,0 +1,117 @@
+import React, { useCallback} from 'react'
+import { useSelector} from 'react-redux';
+
+import classes from './Pagination.module.css'
+
+
+
+
+const PaginationTask = ({page, setPage, limit, middle, setMiddle}) => {
+ 
+  const total = useSelector(state => state.tasks.total);
+  const totalPages = Math.ceil(total / limit)
+
+  const maxMiddle = (Math.ceil(totalPages / 5 - 1) * 5) + 3
+
+  function goToFirstPage() {
+    setMiddle(3);
+    setPage(1);
+  }
+
+  function goToPreviousPage() {
+    setMiddle((middle) => Math.max(3, middle - 5));
+  }
+
+  function goToNextPage() {
+    setMiddle((middle) => Math.min(maxMiddle, middle + 5))
+ 
+  }
+
+  function goToLastPage() {
+    setMiddle(maxMiddle);
+    setPage(totalPages);
+  }
+
+  function changePage(event) {
+    const pageNumber = Number(event.target.textContent);
+    setPage(pageNumber);
+  }
+
+  const getPaginationGroup = useCallback(() => {
+    const start = Math.max(1, middle - 2)
+    const end = Math.min(middle + 2, totalPages)
+    let list = [];
+    for (let i = start; i <= end; i++) {
+      list.push(i);
+    }
+    return list
+  }, [middle, totalPages]);
+
+  
+  return (
+    <div>
+
+      {limit && (
+
+        <div className={classes.pagination}>
+
+          {/* first page button */}
+          {middle > 3 && (
+            <button
+              onClick={goToFirstPage}
+              className={`${classes.paginationItem} 'active' `}        >
+              <span>1</span>
+            </button>
+          )}
+
+
+          {/* previous button */}
+          {middle > 3 && (
+            <button
+              onClick={goToPreviousPage}
+              className={`${classes.prev} ${page === 1 ? 'disabled' : ''}`}
+            >
+              &lt;&lt;
+            </button>
+          )}
+
+          {/* show page numbers */}
+          {getPaginationGroup().map((item, index) => (
+            <button
+              key={index}
+              onClick={changePage}
+              className={`${classes.paginationItem} ${page === item ? 'active' : null}`}
+            >
+              <span>{item}</span>
+            </button>
+          ))}
+
+          {/* next button */}
+
+          {middle < maxMiddle && (
+            <button
+              onClick={goToNextPage}
+              className={`${classes.next} ${page === totalPages ? 'disabled' : ''}`} >
+              &gt;&gt;
+            </button>
+          )}
+
+
+          {/* last page button */}
+          {middle < totalPages && (
+            <button
+              onClick={goToLastPage}
+              className={`${classes.paginationItem} 'active'`}        >
+              <span>{totalPages}</span>
+            </button>
+          )}
+
+        </div>
+
+      )}
+
+    </div>
+  )
+}
+   
+export default PaginationTask
